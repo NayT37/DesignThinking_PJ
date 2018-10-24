1ddac5754aca4c7069580a00f61b7732031cd07b
LuisFJojoa <luis.jojoa.q@gmail.com>
2018-10-24 17:08:12 -0500

    Login

---
+++ Assets/Resources/DoTweenCtrl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DoTweenCtrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		DOTween.Play("1");
+		DOTween.Play("2");
+		DOTween.Play("3");
+		DOTween.Play("4");
+		DOTween.Play("5");
+		DOTween.Play("6");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
