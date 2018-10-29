9737b4da115ca01e3be41fa4b3c41dec208a1645
Freddy Orozco <freddycamilo95@gmail.com>
2018-10-29 15:26:43 -0500

    Making great changes

---
+++ Assets/Resources/CodeTemplates/Main_Template.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Template : MonoBehaviour
+{
+    #region VARIABLES
+    //Public Variables
+    //Private Variables
+    #endregion
+    #region SYSTEM_METHODS
+    public virtual void Start() { Initializate(); }
+    public virtual void Update() { }
+    #endregion
+    #region CREATED_METHODS
+    private void Initializate() { }
+    #endregion
+    #region COROUTINES
+    #endregion
+}
