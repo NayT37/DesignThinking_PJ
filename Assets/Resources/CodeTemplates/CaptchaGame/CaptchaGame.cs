8347cce3a366ee3d07893052bb8a363e77e4d1a7
Freddy Orozco <freddycamilo95@gmail.com>
2018-10-30 16:54:39 -0500

    Working in the captcha game

--- Assets/Resources/CodeTemplates/CaptchaGame/CaptchaGame.cs
+++ Assets/Resources/CodeTemplates/CaptchaGame/CaptchaGame.cs
@@ -8,44 +8,42 @@ public class CaptchaGame : MonoBehaviour
 {
     #region VARIABLES
     //Public Variables
-    //Size of Images to be displayed (show images array) //Enum
+    //Size of Images to be displayed (show images array)
     public enum DisplayedImages { six = 6, eight = 8, nine = 9 };
     public DisplayedImages imagesToDisplay = DisplayedImages.six;
-    //Size of True Images
+    //How many displayed images are in the screen?
     public int correctImageQuantity = 3;
     public float imagesSize = 100f;
     [Range(0.5f, 10.0f)]
     public float horizontalSeparation = 2f;
     [Range(0.5f, 1.9f)]
     public float verticalSeparation = 0.75f;
-    //Path for correct images
-    public string pathForImages;
-    //Empty parent object for images ubication
-    [SerializeField]
-    private Transform _parentObject;
+    public Sprite[] _incorrectImgsArray;
+    public Sprite[] _correctImgsArray;
+
     //Private Variables
-    //General False Images
-    private Sprite[] _incorrectImgsArray;
-    private Sprite[] _correctImgsArray;
-    //General True Images
-    //Show Images Array (True + false) classObj?
+    [SerializeField]
+    //Empty parent object for images ubication
+    private Transform _parentObject;
+
+    private Sprite[] _showedImgsArray;
+    private Vector3[] _storedPositionArray;
+    private GameObject[] _displayedImagesArray;
     #endregion
+
+
     #region SYSTEM_METHODS
     public virtual void Start() { Initializate(); }
     public virtual void Update() { }
     #endregion
+
+
     #region CREATED_METHODS
     private void Initializate()
     {
-        try
-        {
-            _incorrectImgsArray = Resources.LoadAll<Sprite>(pathForImages);
-            _correctImgsArray = Resources.LoadAll<Sprite>(pathForImages);
-        }
-        catch (Exception e)
-        {
-            Debug.Log("Path is incorrect" + e);
-        }
+        _storedPositionArray = new Vector3[(int)imagesToDisplay];
+        //_showedImgsArray = new Sprite[(int)imagesToDisplay];
+        _displayedImagesArray = new GameObject[(int)imagesToDisplay];
 
         switch ((int)imagesToDisplay)
         {
@@ -66,7 +64,9 @@ public class CaptchaGame : MonoBehaviour
                     {
                         NewObj.transform.localPosition = new Vector3((dimension.rect.width + (dimension.rect.width / horizontalSeparation)) * (i - 3) - (dimension.rect.width + (dimension.rect.width / horizontalSeparation)), dimension.rect.height / -verticalSeparation, 0);
                     }
-                    NewObj.SetActive(true);
+                    AssignImageBehaviour(i, NewObj);
+                    _storedPositionArray[i] = NewObj.transform.localPosition;
+                    _displayedImagesArray[i] = NewObj;
                 }
                 break;
             case 8:
@@ -86,13 +86,14 @@ public class CaptchaGame : MonoBehaviour
                     {
                         NewObj.transform.localPosition = new Vector3((dimension.rect.width + (dimension.rect.width / horizontalSeparation)) * (i - 4) - (dimension.rect.width + (dimension.rect.width / horizontalSeparation)) - dimension.rect.width / 2, dimension.rect.height / -verticalSeparation, 0);
                     }
-                    NewObj.SetActive(true);
+                    AssignImageBehaviour(i, NewObj);
+                    _storedPositionArray[i] = NewObj.transform.localPosition;
+                    _displayedImagesArray[i] = NewObj;
                 }
                 break;
             case 9:
                 for (int i = 0; i < (int)imagesToDisplay; i++)
                 {
-                    print(i);
                     GameObject NewObj = new GameObject(); //Create the GameObject
                     Image NewImage = NewObj.AddComponent<Image>(); //Add the Image Component script
                     var dimension = NewObj.GetComponent<RectTransform>();
@@ -111,24 +112,56 @@ public class CaptchaGame : MonoBehaviour
                     {
                         NewObj.transform.localPosition = new Vector3((dimension.rect.width + (dimension.rect.width / horizontalSeparation)) * (i - 6) - (dimension.rect.width + (dimension.rect.width / horizontalSeparation)), dimension.rect.height * 2 / -verticalSeparation, 0);
                     }
-                    NewObj.SetActive(true);
+                    AssignImageBehaviour(i, NewObj);
+                    _storedPositionArray[i] = NewObj.transform.localPosition;
+                    _displayedImagesArray[i] = NewObj;
                 }
                 break;
         }
+        RandomImageArray();
     }
-
-    private void OrderImages(Image[] arr) { }
     //Random Images Array
-
-    public void ValidateAnswer()
+    private void RandomImageArray()
     {
-        //Check Click  //If Ok  //if False
+        //  Vector3[] temp = _storedPositionArray;
+        Vector3 tempObj;
+        for (int i = 0; i < _storedPositionArray.Length; i++)
+        {
+            int rnd = UnityEngine.Random.Range(0, _storedPositionArray.Length);
+            tempObj = _storedPositionArray[rnd];
+            _storedPositionArray[rnd] = _storedPositionArray[i];
+            _storedPositionArray[i] = tempObj;
+            _storedPositionArray[i] = _storedPositionArray[i];
+        }
+        ReassignImagePositions();
     }
+
+    private void ReassignImagePositions()
+    {
+        for (int i = 0; i < _displayedImagesArray.Length; i++)
+        {
+            _displayedImagesArray[i].transform.localPosition = _storedPositionArray[i];
+        }
+    }
+
+    private void AssignImageBehaviour(int index, GameObject obj)
+    {
+        CaptchaImage captchaImage = obj.AddComponent<CaptchaImage>();
+        if (index < correctImageQuantity)
+        {
+            captchaImage.isCorrect = true;
+        }
+        else
+        {
+            captchaImage.isCorrect = false;
+        }
+    }
+
     #endregion
 }
 
 [System.Serializable]
-public class CaptchaImage : System.Object
+public class CaptchaImages : System.Object
 {
     public Sprite imgSprite;
     public bool isCorrect;
