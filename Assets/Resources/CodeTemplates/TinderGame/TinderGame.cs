a7b528e73467a8bf4684c637a80000b6f0171926
Freddy Orozco <freddycamilo95@gmail.com>
2018-11-01 10:19:02 -0500

    Adding Tinder Template

--- Assets/Resources/CodeTemplates/TinderGame/TinderGame.cs
+++ Assets/Resources/CodeTemplates/TinderGame/TinderGame.cs
@@ -2,15 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TinderGame : MonoBehaviour {
+public class TinderGame : MonoBehaviour
+{
+    #region VARIABLES
+    //Public Variables
+    public int imagesQuantity;
+    //Private Variables
+    public List<TinderImage> _tinderImagesList;
+    #endregion
+    #region SYSTEM_METHODS
+    private void Start() { Initializate(); }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ValidateAnswer(false);
+        }
+    }
+    #endregion
+    #region CREATED_METHODS
+    private void Initializate()
+    {
+        _tinderImagesList = new List<TinderImage>();
+        for (int i = 0; i < imagesQuantity; i++)
+        {
+            TinderImage item = new TinderImage();
+            //item.internalImage.sprite = 
+            //item.isCorrect =
+            _tinderImagesList.Add(item);
+        }
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void UpdateDisplayedImage()
+    {
+
+    }
+
+    private void ValidateAnswer(bool some)
+    {
+        if (some)
+        { //Correct case
+            _tinderImagesList.RemoveAt(0);
+        }
+        else
+        {
+            TinderImage temp = _tinderImagesList[0];
+            _tinderImagesList.RemoveAt(0);
+            _tinderImagesList.Insert(0, temp);
+        }
+    }
+    #endregion
+    #region COROUTINES
+    #endregion
 }
---
+++ Assets/Resources/CodeTemplates/TinderGame/TinderImage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TinderImage : MonoBehaviour
+{
+    #region VARIABLES
+    //Public Variables
+    [HideInInspector]
+    public Image internalImage;
+    [HideInInspector]
+    public bool isCorrect;
+    //Private Variables
+    #endregion
+    #region SYSTEM_METHODS
+    private void Start() { Initializate(); }
+    private void Update() { }
+    #endregion
+    #region CREATED_METHODS
+    private void Initializate() {}
+    #endregion
+    #region COROUTINES
+    #endregion
+}
