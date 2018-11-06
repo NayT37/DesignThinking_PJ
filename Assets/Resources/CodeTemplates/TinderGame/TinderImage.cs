009b840d94e3d715e964d6a7afac2082d91244cc
Freddy Orozco <freddycamilo95@gmail.com>
2018-11-06 15:20:20 -0500

    Tinder Template is now ready
    Added a virtual void named "FinishGame", this void can be overwritten to know the finish game behaviour

--- Assets/Resources/CodeTemplates/TinderGame/TinderImage.cs
+++ Assets/Resources/CodeTemplates/TinderGame/TinderImage.cs
@@ -4,15 +4,30 @@ using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TinderImage : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class TinderImage : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     #region VARIABLES
     //Public Variables
     [HideInInspector]
     public Image internalImage;
     [HideInInspector]
-    public bool isCorrect;
+    public Sprite spriteToShow;
+    [HideInInspector]
+    public bool internalAnswer;
+    [HideInInspector]
+    public bool selectedAnswer;
+    [HideInInspector]
+    public float answerAxis;
+
     //Private Variables
+    private float _yAxis;
+    private Vector3 _originalPos;
+    private TinderGame _gameCtrl;
+
+
+    //Event Variables
+    public delegate void ValidateAction();
+    public static event ValidateAction ValidateImage;
     #endregion
 
 
@@ -23,21 +38,61 @@ public class TinderImage : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 
 
     #region CREATED_METHODS
-    private void Initializate() { }
+    private void Initializate()
+    {
+        _originalPos = transform.position;
+        _yAxis = _originalPos.y;
+        internalImage = GetComponent<Image>();
+        answerAxis = 0;
+        internalImage.sprite = spriteToShow;
+        _gameCtrl = GameObject.FindObjectOfType<TinderGame>();
+    }
+
+    public void UpdateSprite(Sprite newSprite)
+    {
+        internalImage.sprite = newSprite;
+    }
     #endregion
 
 
     #region INTERFACE_METHODS
-
-    public void OnPointerDown(PointerEventData eventData)
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        print(eventData.position);
+
+        internalImage.raycastTarget = false;
 
     }
 
-    public void OnPointerUp(PointerEventData eventData)
+    public void OnDrag(PointerEventData eventData)
     {
-        print(eventData);
+
+        float tempX = Input.mousePosition.x;
+        transform.position = new Vector3(Input.mousePosition.x, _yAxis - (Mathf.Abs(transform.localPosition.x) / 10), 0);
+        transform.eulerAngles = new Vector3(0, 0, (-transform.localPosition.x / 25));
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+        answerAxis = transform.localPosition.x;
+        if (answerAxis > 0)
+        {
+            _gameCtrl.selectedAnswer = true;
+            ValidateImage();
+        }
+        else if (answerAxis < 0)
+        {
+            _gameCtrl.selectedAnswer = false;
+            ValidateImage();
+        }
+        else if (answerAxis == 0)
+        {
+            Debug.Log("Image it wasn't moved");
+        }
+        internalImage.raycastTarget = true;
+        transform.position = _originalPos;
+        transform.eulerAngles = new Vector3(0, 0, 0);
 
     }
     #endregion
