2dfeb2a6c7f7ca4a4c4d4bd727860327eb3de467
juan correa <juansecorrea12@gmail.com>
2018-11-22 11:47:59 -0500

    cambios en el HUD y creación de carpetas por scena de los casos

--- Assets/Tools/HUD/Scripts/ChangeSelectBtn.cs
+++ Assets/Tools/HUD/Scripts/ChangeSelectBtn.cs
@@ -3,40 +3,94 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEditor;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 
 public class ChangeSelectBtn : MonoBehaviour {
 
+	//Variables dentro del HUD
+	private Image Empatizar_btn;
+	private Image Definir_btn;
+	private Image Idear_btn;
+	private Image Prototipar_btn;
+	private Image Probar_btn;
+
+
+	//Variables correspondientes al cambio de escenas
+	[Header("EMPATIZAR")]
+	public Sprite EmpatizarPointer;
+	public Sprite EmpatizarSelected;
+	[Header("DEFINIR")]
+	public Sprite DefinirNull;
+	public Sprite DefinirPointer;
+	public Sprite DefinirSelected;
+	[Header("IDEAR")]
+	public Sprite IdearNull;
+	public Sprite IdearPointer;
+	public Sprite IdearSelected;
+	[Header("PROTOTIPAR")]
+	public Sprite PrototiparPointer;
+	public Sprite PrototiparSelected;
+	[Header("PROBAR")]
+	public Sprite ProbarNull;
+	public Sprite ProbarSelected;
 
 
 
-	public Image ImageMoment1_null;
-	public Image ImageMoment1_pointer;
-	public Image ImageMoment1_selected;
-	public Image ImageMoment2_null;
-	public Image ImageMoment2_pointer;
-	public Image ImageMoment2_selected;
-	public Image ImageMoment3_null;
-	public Image ImageMoment3_pointer;
-	public Image ImageMoment3_selected;
-	public Image ImageMoment4_pointer;
-	public Image ImageMoment4_selected;
-	public Image ImageMoment5_null;
-	public Image ImageMoment5_selected;
-
-
-
-
-
-
-
-	// Use this for initialization
 	void Start () {
-		
+		//start animation
+		DOTween.Play ("5");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void SelectEmpatizar(){
+		Empatizar_btn = GameObject.Find ("Empatizar").GetComponent<Image>();
+		Empatizar_btn.sprite = EmpatizarSelected;
+		Definir_btn = GameObject.Find ("Definir").GetComponent<Image> ();
+		Definir_btn.sprite = DefinirNull;
+	}
+
+
+	public void SelectDefinir(){
+		Definir_btn = GameObject.Find ("Definir").GetComponent<Image> ();
+		Definir_btn.sprite = DefinirSelected;
+		Empatizar_btn = GameObject.Find ("Empatizar").GetComponent<Image>();
+		Empatizar_btn.sprite = EmpatizarPointer;
+	}
+
+
+	public void SelectIdear(){
+		Idear_btn = GameObject.Find ("Idear").GetComponent<Image> ();
+		Idear_btn.sprite = IdearSelected;
+		Definir_btn = GameObject.Find ("Definir").GetComponent<Image> ();
+		Definir_btn.sprite = DefinirPointer;
+		Empatizar_btn = GameObject.Find ("Empatizar").GetComponent<Image>();
+		Empatizar_btn.sprite = EmpatizarPointer;
+	}
+
+
+	public void SelectPrototipar(){
+		Prototipar_btn = GameObject.Find ("Prototipar").GetComponent<Image> ();
+		Prototipar_btn.sprite = PrototiparSelected;
+		Idear_btn = GameObject.Find ("Idear").GetComponent<Image> ();
+		Idear_btn.sprite = IdearPointer;
+		Definir_btn = GameObject.Find ("Definir").GetComponent<Image> ();
+		Definir_btn.sprite = DefinirPointer;
+		Empatizar_btn = GameObject.Find ("Empatizar").GetComponent<Image>();
+		Empatizar_btn.sprite = EmpatizarPointer;
+	}
+
+
+	public void SelectProbar(){
+		Probar_btn = GameObject.Find ("Probar").GetComponent<Image> ();
+		Probar_btn.sprite = ProbarSelected;
+		Prototipar_btn = GameObject.Find ("Prototipar").GetComponent<Image> ();
+		Prototipar_btn.sprite = PrototiparPointer;
+		Idear_btn = GameObject.Find ("Idear").GetComponent<Image> ();
+		Idear_btn.sprite = IdearPointer;
+		Definir_btn = GameObject.Find ("Definir").GetComponent<Image> ();
+		Definir_btn.sprite = DefinirPointer;
+		Empatizar_btn = GameObject.Find ("Empatizar").GetComponent<Image>();
+		Empatizar_btn.sprite = EmpatizarPointer;
 	}
 }
