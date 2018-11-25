2933614fc9f1d97ec6d30e7cf5fdb0f6b878aaba
Juan Camilo Trillos <juanktrillos@users.noreply.github.com>
2018-11-25 16:31:24 -0500

    testear finish

---
+++ Assets/Prototype_CT/Scripts/IntroScript/dropContent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dropContent : DropBehavior {
+
+	public GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+		panel.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public override void OnDrop (UnityEngine.EventSystems.PointerEventData eventData) {
+		if (!internalItem) {
+			panel.SetActive (true);
+		}
+	}
+}
