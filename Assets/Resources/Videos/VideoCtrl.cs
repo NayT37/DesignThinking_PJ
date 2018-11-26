4c3f65b20eaa4cbfdf18a3f1ff81fdfa880016f5
Freddy Orozco <freddycamilo95@gmail.com>
2018-11-26 15:56:59 -0500

    Before Build

---
+++ Assets/Resources/Videos/VideoCtrl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoCtrl : MonoBehaviour
+{
+
+    public VideoPlayer videoPlayer;
+    public GameObject quadObj;
+    private CasesHUD_Ctrl casesCtrl;
+    private GameObject closeBtn, openBtn;
+
+    // Use this for initialization
+    void Start()
+    {
+        closeBtn = GameObject.Find("BtnClose");
+        openBtn = GameObject.Find("BtnPlay");
+        closeBtn.SetActive(false);
+        casesCtrl = FindObjectOfType<CasesHUD_Ctrl>();
+        quadObj.SetActive(false);
+    }
+
+    public void StartVideo()
+    {
+        quadObj.SetActive(true);
+        videoPlayer.Play();
+        closeBtn.SetActive(true);
+        openBtn.SetActive(false);
+    }
+    public void CloseVideo()
+    {
+        videoPlayer.Stop();
+        quadObj.SetActive(false);
+        closeBtn.SetActive(false);
+        openBtn.SetActive(true);
+    }
+
+}
