537547519689653b81fb4a22b46d67ec61c842b8
juan correa <juansecorrea12@gmail.com>
2018-11-25 23:48:36 -0500

    Update Project

---
+++ Assets/_Scenes/MainScene/Ctrl_MainScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Ctrl_MainScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void GoUser(){
+		StartCoroutine (ResgisterUser ());
+	}
+
+	IEnumerator ResgisterUser(){
+		SceneManager.LoadScene ("RegisterUser", LoadSceneMode.Additive);
+		yield return null;
+		SceneManager.UnloadSceneAsync ("Main_Scene");
+	}
+}
