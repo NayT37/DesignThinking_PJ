b00fe2266f5e965856af023f770b3bbbce7f5c6d
juan correa <juansecorrea12@gmail.com>
2018-11-26 00:16:54 -0500

    update this thing

---
+++ Assets/_Scenes/RegisterScene/Ctrl_RegisterUser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Ctrl_RegisterUser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void GoUser(){
+		StartCoroutine (ResgisterUser ());
+	}
+
+	IEnumerator ResgisterUser(){
+		SceneManager.LoadScene ("SelectGame", LoadSceneMode.Additive);
+		yield return null;
+		SceneManager.UnloadSceneAsync ("Register_User");
+	}
+}
