bb9ef571b75986c909670519778c2e978f718a08
juan correa <juansecorrea12@gmail.com>
2018-11-26 15:56:52 -0500

    update

---
+++ Assets/_Scenes/LoadGame_Page/Scripts/Ctrl_LoadGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ctrl_LoadGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void back(){
+		StartCoroutine (GoBack ());
+	}
+
+	IEnumerator GoBack(){
+		SceneManager.LoadScene ("CreateGroup", LoadSceneMode.Additive);
+		yield return null;
+		SceneManager.UnloadSceneAsync ("LoadGame");
+	}
+}
