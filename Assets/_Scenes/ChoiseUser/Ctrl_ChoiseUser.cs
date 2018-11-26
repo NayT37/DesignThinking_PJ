981b8eb4400214e753d57eab925b89690463a037
juan correa <juansecorrea12@gmail.com>
2018-11-26 03:00:05 -0500

    new scene

---
+++ Assets/_Scenes/ChoiseUser/Ctrl_ChoiseUser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Ctrl_ChoiseUser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void GoChallenge(){
+		StartCoroutine (Challenge ());
+	}
+	public void GoTrainning(){
+		StartCoroutine (Trainning());
+	}
+
+	IEnumerator Challenge(){
+		SceneManager.LoadScene ("Activities", LoadSceneMode.Additive);
+		yield return null;
+		SceneManager.UnloadSceneAsync ("ChoiseUser");
+	}
+	IEnumerator Trainning(){
+		SceneManager.LoadScene ("Main_HUD", LoadSceneMode.Additive);
+		yield return null;
+		SceneManager.UnloadSceneAsync ("ChoiseUser");
+	}
+}
