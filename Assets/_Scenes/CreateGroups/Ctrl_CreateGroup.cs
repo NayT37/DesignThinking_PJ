0e71ae48c84045a0b8de3f98b5248e6b51eaf6fd
juan correa <juansecorrea12@gmail.com>
2018-11-26 08:39:55 -0500

    update

---
+++ Assets/_Scenes/CreateGroups/Ctrl_CreateGroup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl_CreateGroup : MonoBehaviour {
+
+
+	private string TitleCurse;
+
+	void Start () {
+		
+	}
+	
+
+}
