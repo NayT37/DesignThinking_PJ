6c8ab8564840f45530abaab0432b2e2ab2f884b3
Juan Sebastián Correa Villegas <juansecorrea12@gmail.com>
2018-12-06 11:15:51 -0500

    Update

---
+++ Assets/_Scenes/CreateCurso/CtrlCreateCurso.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CtrlCreateCurso : MonoBehaviour {
+
+
+	#region Variables
+	public InputField NameCourse;
+	private string tmp;
+	#endregion
+
+	void Start () {
+		
+	}
+
+	void Update(){
+		
+//		Debug.Log ("Mensaje" + tmp);
+	}
+
+	public void Close(){
+		StartCoroutine (Back());
+	}
+
+	public void SaveData(){
+		tmp = NameCourse.text;
+		StartCoroutine (SaveNameCourse ());
+	}
+		
+	IEnumerator Back(){
+		SceneManager.LoadScene ("SelectGame", LoadSceneMode.Additive);
+		yield return null;
+		SceneManager.UnloadSceneAsync ("SelectActivity");
+	}
+
+	IEnumerator SaveNameCourse(){
+		Main_Ctrl.instance.NameCourse = NameCourse.text;
+		SceneManager.LoadScene ("CreateGroup", LoadSceneMode.Additive);
+		yield return null;
+		SceneManager.UnloadSceneAsync ("SelectActivity");
+	}
+
+}
