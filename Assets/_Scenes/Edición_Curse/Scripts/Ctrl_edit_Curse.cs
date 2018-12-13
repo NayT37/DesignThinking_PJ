c5389a8440128b8bdebfd3f6b3879f6174534caf
juan correa <juansecorrea12@gmail.com>
2018-12-13 09:10:56 -0500

    scripts de las escenas

---
+++ Assets/_Scenes/Edición_Curse/Scripts/Ctrl_edit_Curse.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ctrl_edit_Curse : MonoBehaviour {
+
+
+
+
+	void Start () {
+		
+	}
+
+
+	void Update () {
+		
+	}
+
+	public void editBtn(){
+		StartCoroutine (GotoScene ());
+	}
+
+	public void backScene(){
+		StartCoroutine (GobackScene ());
+	}
+
+	IEnumerator GotoScene(){
+		SceneManager.LoadScene ("Edit_Group", LoadSceneMode.Additive);
+		yield return null;
+		SceneManager.UnloadSceneAsync ("Edit_Curse");
+	}
+
+	IEnumerator GobackScene(){
+		SceneManager.LoadScene ("LoadGame", LoadSceneMode.Additive);
+		yield return null;
+		SceneManager.UnloadSceneAsync ("Edit_Curse");
+	}
+}
