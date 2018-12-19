797945d25a63e76f056b82a8e7de0f607f549982
juan correa <juansecorrea12@gmail.com>
2018-12-19 16:31:47 -0500

    update prefabs

---
+++ Assets/_Scenes/Edición_Curse/Scripts/slider_EditCurse.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class slider_EditCurse : MonoBehaviour, IPointerClickHandler, IPointerDownHandler {
+
+
+	public float slider_value;
+	private Slider _sliderEditCurse;
+
+	void Start () {
+		_sliderEditCurse = GetComponent<Slider> ();
+		slider_value = 0;
+	}
+
+	void Update () {
+		
+	}
+
+	#region IPointerClickHandler implementation
+
+	public void OnPointerClick (PointerEventData eventData)
+	{
+		slider_value = _sliderEditCurse.value;
+	}
+
+	#endregion
+
+	#region IPointerDownHandler implementation
+
+	public void OnPointerDown (PointerEventData eventData)
+	{
+		slider_value = _sliderEditCurse.value;	
+	}
+
+	#endregion
+}
