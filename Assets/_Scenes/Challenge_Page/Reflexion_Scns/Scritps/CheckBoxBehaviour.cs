587894a02064aeaa78ea8c7cefc4cec9e182d1c4
Freddy Orozco <freddycamilo95@gmail.com>
2018-12-19 16:03:45 -0500

    Intro almost ready

---
+++ Assets/_Scenes/Challenge_Page/Reflexion_Scns/Scritps/CheckBoxBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CheckBoxBehaviour : MonoBehaviour, IPointerClickHandler
+{
+
+
+    #region VARIABLES
+    //Public Variables
+    //Private Variables
+    GameObject _checkObj;
+    #endregion
+
+
+    #region SYSTEM_METHODS
+    private void Start() { Initializate(); }
+    private void Update() { }
+    #endregion
+
+
+    #region CREATED_METHODS
+    private void Initializate()
+    {
+        _checkObj = transform.Find("CheckBox").GetChild(0).gameObject;
+        _checkObj.SetActive(false);
+    }
+    #endregion
+
+
+    #region INTERFACE_METHODS
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        _checkObj.SetActive(!_checkObj.activeInHierarchy);
+    }
+    #endregion
+
+
+    #region GETTERS_AND_SETTERS
+    #endregion
+
+
+    #region COROUTINES
+    #endregion
+}
---
+++ Assets/_Scenes/Challenge_Page/Reflexion_Scns/Scritps/ReflexionCtrl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReflexionCtrl : MonoBehaviour
+{
+
+    #region VARIABLES
+    //Public Variables
+    public int itemsQuantity;
+    //Private Variables
+    private GameObject[] _itemList;
+    private Button _nextBtn;
+    #endregion
+
+
+    #region SYSTEM_METHODS
+    private void Start()
+    { Initializate(); }
+    private void Update() { }
+    #endregion
+
+
+    #region CREATED_METHODS
+    private void Initializate()
+    {
+        _nextBtn = GameObject.Find("Next_Btn").GetComponent<Button>();
+        _nextBtn.onClick.AddListener(NextStage);
+        _itemList = new GameObject[itemsQuantity];
+        for (int i = 0; i < _itemList.Length; i++)
+        {
+            _itemList[i] = GameObject.Find("Intro_" + (i + 1));
+            if (i != 0)
+            {
+                //     _itemList[i].SetActive(false);
+            }
+        }
+    }
+
+    private void NextStage()
+    {
+        print("something");
+    }
+    #endregion
+
+
+    #region INTERFACE_METHODS
+    #endregion
+
+
+    #region GETTERS_AND_SETTERS
+    #endregion
+
+
+    #region COROUTINES
+    #endregion
+}
