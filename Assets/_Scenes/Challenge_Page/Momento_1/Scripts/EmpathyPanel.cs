f868fe3e751dbec8ff13e2538d883f7a19444d3f
Freddy Orozco <freddycamilo95@gmail.com>
2018-12-23 12:44:59 -0500

    M_1 empathy map is now complete

---
+++ Assets/_Scenes/Challenge_Page/Momento_1/Scripts/EmpathyPanel.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EmpathyPanel : MonoBehaviour
+{
+    #region VARIABLES
+    //Public Variables
+    //Private Variables
+    [SerializeField]
+    private bool _isMainPanel;
+    private bool _isActivePanel;
+    private bool _isInfoEmpty;
+    private string _internalText;
+    private InputField _internalInput;
+    private Button _saveInfoBtn;
+    private M1_Ctrl _levelCtrl;
+    private int _panelId;
+    #endregion
+
+
+    #region SYSTEM_METHODS
+    private void Awake() { Initializate(); }
+    #endregion
+
+
+    #region CREATED_METHODS
+    private void Initializate()
+    {
+        _isActivePanel = false;
+        _isInfoEmpty = false;
+        _internalText = "";
+        _panelId = int.Parse(name.Split('_')[1]);
+        if (!_isMainPanel)
+        {
+            _internalInput = transform.Find("InputTextHolder").GetChild(0).GetComponent<InputField>();
+            _saveInfoBtn = transform.Find("BtnGuardar").GetComponent<Button>();
+            _saveInfoBtn.onClick.AddListener(SaveInfo);
+        }
+    }
+
+    private void SaveInfo()
+    {
+
+
+        if (_levelCtrl == null)
+        {
+            _levelCtrl = GameObject.FindObjectOfType<M1_Ctrl>();
+        }
+        if (_internalInput.text != "" && _internalInput.text != _internalText)
+        {
+            _internalText = _internalInput.text;
+            _isInfoEmpty = false;
+            _levelCtrl.OnPanelTextChanged(_panelId);
+        }
+        _levelCtrl.ActivePanelByNumber(0);
+    }
+    #endregion
+
+
+    #region INTERFACE_METHODS
+    #endregion
+
+
+    #region GETTERS_AND_SETTERS
+    public void SetActivePanel(bool value)
+    {
+        _isActivePanel = value;
+        this.gameObject.SetActive(_isActivePanel);
+    }
+    #endregion
+
+
+    #region COROUTINES
+    #endregion
+}
---
+++ Assets/_Scenes/Challenge_Page/Momento_1/Scripts/M1_Ctrl.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M1_Ctrl : MonoBehaviour
+{
+    #region VARIABLES
+    //Public Variables
+    //Private Variables
+    [SerializeField]
+    private int _panelQuantity;
+    private EmpathyPanel[] _panelsArray;
+    private GameObject[] _checkByPanel;
+    private int _activePanel;
+    #endregion
+
+
+    #region SYSTEM_METHODS
+    private void Start() { Initializate(); }
+    private void Update() { }
+    #endregion
+
+
+    #region CREATED_METHODS
+    private void Initializate()
+    {
+        _panelsArray = new EmpathyPanel[_panelQuantity];
+        _checkByPanel = new GameObject[_panelQuantity - 1];
+
+        for (int i = 0; i < _panelsArray.Length; i++)
+        {
+            _panelsArray[i] = GameObject.Find("Panel_" + i).GetComponent<EmpathyPanel>();
+            if (i > 0)
+            {
+                _panelsArray[i].SetActivePanel(false);
+            }
+        }
+        _activePanel = 0;
+        for (int i = 0; i < _checkByPanel.Length; i++)
+        {
+            _checkByPanel[i] = GameObject.Find("Zones").transform.GetChild(i).Find("CheckObj").gameObject;
+            _checkByPanel[i].SetActive(false);
+        }
+
+    }
+
+    public void ActivePanelByNumber(int panelNumber)
+    {
+        if (panelNumber != _activePanel)
+        {
+            _panelsArray[_activePanel].SetActivePanel(false);
+            _panelsArray[panelNumber].SetActivePanel(true);
+            _activePanel = panelNumber;
+        }
+    }
+
+    public void OnPanelTextChanged(int panelId)
+    {
+        _checkByPanel[panelId - 1].SetActive(true);
+    }
+    #endregion
+
+
+    #region INTERFACE_METHODS
+    #endregion
+
+
+    #region GETTERS_AND_SETTERS
+    #endregion
+
+
+    #region COROUTINES
+    #endregion
+}
