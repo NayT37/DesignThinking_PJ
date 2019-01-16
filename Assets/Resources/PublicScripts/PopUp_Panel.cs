d826892a30497878f70b1d25c10ae193b0f78090
Freddy Orozco <freddycamilo95@gmail.com>
2019-01-16 14:35:40 -0500

    PopUp panel was added

---
+++ Assets/Resources/PublicScripts/PopUp_Panel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+
+public class PopUp_Panel : MonoBehaviour, IPointerClickHandler
+{
+    #region VARIABLES
+    //Public Variables
+    //Private Variables
+    private bool _canClose;
+    private GameObject _closeBtn;
+    #endregion
+
+
+    #region SYSTEM_METHODS
+    private void Start() { Initializate(); }
+    private void Update() { }
+    #endregion
+
+
+    #region CREATED_METHODS
+    private void Initializate()
+    {
+        _canClose = false;
+        _closeBtn = transform.Find("CloseBtn").gameObject;
+        _closeBtn.SetActive(false);
+        StartCoroutine(WaitUntilLoad());
+    }
+
+    private void ClosePanel()
+    {
+        if (_canClose)
+            this.gameObject.SetActive(false);
+    }
+    #endregion
+
+
+    #region INTERFACE_METHODS
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        ClosePanel();
+    }
+    #endregion
+
+
+    #region GETTERS_AND_SETTERS
+    #endregion
+
+
+    #region COROUTINES
+    private IEnumerator WaitUntilLoad()
+    {
+        yield return new WaitForSeconds(2);
+        _canClose = true;
+        _closeBtn.SetActive(true);
+    }
+
+    #endregion
+}
