417187961a026f150bb73e586204b10c1d4cd25e
juan correa <juansecorrea12@gmail.com>
2019-01-22 11:45:07 -0500

    panel instrucciones 100%

--- Assets/Resources/PublicScripts/PanelBackground.cs
+++ Assets/Resources/PublicScripts/PanelBackground.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class PanelBackground : MonoBehaviour, IPointerClickHandler
+public class PanelBackground : MonoBehaviour
 {
 	#region VARIABLES
 	//Public Variables
 	//Private Variables
+	public GameObject canvasInstruction;
 	private bool _canClose;
 	private GameObject _nextBtn;
 	private GameObject _backBtn;
+	private GameObject _exitBtn;
 	private Text _titleText;
 	private Text _internalText;
 	private int Round = 0;
+
+	public GameObject Emp;
+	public GameObject Def;
+	public GameObject Ide;
+	public GameObject Pro;
+	public GameObject Eva;
+
+
+	private Color32 _originalColor = new Color32 (255, 255, 255, 255);
+	private Color32 _newColor = new Color32 (255, 255, 255, 37);
 	#endregion
 
 
@@ -34,26 +46,33 @@ public class PanelBackground : MonoBehaviour, IPointerClickHandler
 		_canClose = false;
 		_nextBtn = transform.Find("NextBtn").gameObject;
 		_backBtn = transform.Find("BackBtn").gameObject;
+		_exitBtn = transform.Find ("ExitCanvas").gameObject;
+		_exitBtn.SetActive (false);
 		_backBtn.SetActive(false);
 	}
 	private void CalculateRound(){
+		int contador = 0;
 		switch(Round){
 		case 1:
 			_titleText.GetComponent<Text> ().text = "EMPATIZAR";
 			_internalText.text = "Recuerda debemos ser capaces de ponernos en los zapatos de nuestro " +
 			"público objetivo para ser capaces de generar soluciones consecuentes con sus realidades.";
 			_backBtn.SetActive (false);
+			Emp.GetComponent<Image> ().color = _originalColor;
 			break;
 		case 2: 
 			_titleText.GetComponent<Text> ().text = "DEFINIR";
 			_internalText.text = "A partir de la información recopilada durante la fase Empatizar, debemos " +
 				"quedarnos con lo que realmente aporta valor y nos lleva al alcance de nuevas perspectivas interesantes.";
 			_backBtn.SetActive (true);
+			Def.GetComponent<Image> ().color = _originalColor;
+
 			break;
 		case 3:
 			_titleText.GetComponent<Text> ().text = "IDEAR";
 			_internalText.text = "A partir de la delimitación de nuestro interés, en esta fase  generaremos un sinfín " +
 				"de opciones. No debemos quedarnos con la primera idea que se nos ocurra.";
+			Ide.GetComponent<Image> ().color = _originalColor;
 			break;
 		case 4:
 			_titleText.GetComponent<Text> ().text = "PROTOTIPAR";
@@ -61,6 +80,7 @@ public class PanelBackground : MonoBehaviour, IPointerClickHandler
 			"hacer las ideas palpables y  visualizar las posibles soluciones, poniendo de manifiesto elementos que debemos " +
 			"mejorar o refinar antes de llegar al resultado final.";
 			_nextBtn.SetActive (true);
+			Pro.GetComponent<Image> ().color = _originalColor;
 			break;
 		case 5: 
 			_titleText.GetComponent<Text> ().text = "EVALUAR";
@@ -68,31 +88,16 @@ public class PanelBackground : MonoBehaviour, IPointerClickHandler
 			"que estemos desarrollando, para identificar mejoras significativas, fallos a resolver, posibles carencias. " +
 			"Durante esta fase evolucionaremos nuestra idea hasta convertirla en la solución que estábamos buscando.";
 			_nextBtn.SetActive (false);
+			Eva.GetComponent<Image> ().color = _originalColor;
+			_exitBtn.SetActive (true);
 			break;
+		}
+		if (contador == 1) {
+			
 		}
 	}
 
-	private void ClosePanel()
-	{
-		if (_canClose)
-			this.gameObject.SetActive(false);
-	}
 	#endregion
-
-
-	#region INTERFACE_METHODS
-	public void OnPointerClick(PointerEventData eventData)
-	{
-		ClosePanel();
-	}
-	#endregion
-
-
-	#region GETTERS_AND_SETTERS
-	#endregion
-
-
-
 	#region METHOD FOR BUTTONS
 	public void OnNextBtn(){
 		Round += 1;
@@ -100,6 +105,11 @@ public class PanelBackground : MonoBehaviour, IPointerClickHandler
 	public void OnBackBtn(){
 		Round -= 1;
 	}
+	public void closePanel(){
+		canvasInstruction.SetActive (false);
+	}
 	#endregion
+
+
 }
 
