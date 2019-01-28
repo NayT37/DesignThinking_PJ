d717dc8d8e4982340d62b5648bf00e0c778d04eb
Freddy Orozco <freddycamilo95@gmail.com>
2019-01-27 19:53:15 -0500

    Made some changes to Sapere :alien:
    Responsive was added in some cases
    Load project was changed so it shows projects in the challenge screen
    FeedbackText in M3 was fixed
    A ">" symbol were added un M4

--- Assets/_Scenes/FirstScnPJ/Scripts/Ctrl_LoadProjects.cs
+++ Assets/_Scenes/FirstScnPJ/Scripts/Ctrl_LoadProjects.cs
@@ -5,122 +5,134 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using DG.Tweening;
 
-public class Ctrl_LoadProjects : MonoBehaviour {
+public class Ctrl_LoadProjects : MonoBehaviour
+{
 
 
-	public GameObject prefab_project;
-	public GameObject parent_project;
+    public GameObject prefab_project;
+    public GameObject parent_project;
 
-	public GameObject _validationDelete, _msgDelete;
+    public GameObject _validationDelete, _msgDelete;
 
-	public Transform _parentValidation, _textParent;
+    public Transform _parentValidation, _textParent;
 
-	private ProjectServices _projectServices;
-	private Text _nameConstant;
+    private ProjectServices _projectServices;
+    private Text _nameConstant;
 
-	private Project[] _projects;
+    private Project[] _projects;
 
-	private GameObject[] _prefabsProjects;
+    private GameObject[] _prefabsProjects;
 
-	private int groupid;
+    private int groupid;
 
 
-	// Use this for initialization
-	void Start () {
-		
-		
-		// for (int i = 0; i < 8; i++)
-		// {
-		// 	Instantiate (prefab_project, parent_project.transform);
-		// }
+    // Use this for initialization
+    void Start()
+    {
 
-		// _projectServices = new ProjectServices ();
-		groupid = DataBaseParametersCtrl.Ctrl._groupLoaded.id;
 
-		_prefabsProjects = new GameObject[0];
-		
-		
-	}
+        // for (int i = 0; i < 8; i++)
+        // {
+        // 	Instantiate (prefab_project, parent_project.transform);
+        // }
 
-	
-	void Update(){
+        //_projectServices = new ProjectServices();
+        groupid = DataBaseParametersCtrl.Ctrl._groupLoaded.id;
 
-		
-	}
+        _prefabsProjects = new GameObject[0];
 
-	void LoadAllPrefabs(){
+        LoadAllPrefabs();
+    }
 
-		int lengthProjects = _prefabsProjects.Length;
 
-		if (lengthProjects!= 0)
-		{
-			DeletePrefabs(lengthProjects);
-		}
+    void Update()
+    {
 
-		int counterProjects = _projectServices.GetProjectsCounter(groupid);
 
-		_projects = new Project[counterProjects];
+    }
 
-		_prefabsProjects = new GameObject[counterProjects];
+    void LoadAllPrefabs()
+    {
 
-		var projects = _projectServices.GetProjects();
+        int lengthProjects = _prefabsProjects.Length;
 
-		int counter = 0;
-		foreach (var item in projects) {
-			var SetName = Instantiate (prefab_project, parent_project.transform);
-			SetName.name = counter.ToString();
-			_projects [counter] = item;
-			counter++;
-			SetName.GetComponentInChildren<Text> ().text = item.name;
-			SetName.transform.GetChild(0).GetComponentInChildren<Button> ().onClick.AddListener (delegate{GetProjectPressed (SetName.name, item.name);});
-			SetName.transform.GetChild(1).GetComponentInChildren<Button> ().onClick.AddListener (delegate{DeleteProjectPressed (SetName.name);});
+        if (lengthProjects != 0)
+        {
+            DeletePrefabs(lengthProjects);
+        }
 
-			Debug.Log ("name" + item.name);
-		}
-	}
+        int counterProjects = _projectServices.GetProjectsCounter(groupid);
 
-	void DeletePrefabs(int count){
+        _projects = new Project[counterProjects];
 
-		for (int i = 0; i < count; i++)
-		{
-			DestroyImmediate(_prefabsProjects[i]);
-		}
-	}
+        _prefabsProjects = new GameObject[counterProjects];
 
-	void GetProjectPressed(string positionInToArray, string nameCourse) {
+        var projects = _projectServices.GetProjects();
 
-		int value = int.Parse (positionInToArray);
-		Debug.Log ("position " + positionInToArray);
+        int counter = 0;
+        foreach (var item in projects)
+        {
+            var SetName = Instantiate(prefab_project, parent_project.transform);
+            SetName.name = counter.ToString();
+            _projects[counter] = item;
+            counter++;
+            SetName.GetComponentInChildren<Text>().text = item.name;
+            SetName.transform.GetChild(0).GetComponentInChildren<Button>().onClick.AddListener(delegate { GetProjectPressed(SetName.name, item.name); });
+            SetName.transform.GetChild(1).GetComponentInChildren<Button>().onClick.AddListener(delegate { DeleteProjectPressed(SetName.name); });
 
-		DataBaseParametersCtrl.Ctrl._projectLoaded = _projects[value];
+            Debug.Log("name" + item.name);
+        }
+    }
 
-		DOTween.Play("bg_transition");
-		StartCoroutine(goToScene ());
-	}
+    void DeletePrefabs(int count)
+    {
 
-	public void DeleteProjectPressed(string positionInToArray) {
+        for (int i = 0; i < count; i++)
+        {
+            DestroyImmediate(_prefabsProjects[i]);
+        }
+    }
 
-		int value = int.Parse (positionInToArray);
-		//DataBaseParametersCtrl.Ctrl._projectLoaded = _projects[value];
+    void GetProjectPressed(string positionInToArray, string nameCourse)
+    {
 
-		Button[] _btns = new Button[2];
+        int value = int.Parse(positionInToArray);
+        Debug.Log("position " + positionInToArray);
+
+        DataBaseParametersCtrl.Ctrl._projectLoaded = _projects[value];
+
+        DOTween.Play("bg_transition");
+        StartCoroutine(goToScene());
+    }
+
+    public void DeleteProjectPressed(string positionInToArray)
+    {
+
+        int value = int.Parse(positionInToArray);
+        //DataBaseParametersCtrl.Ctrl._projectLoaded = _projects[value];
+
+        Button[] _btns = new Button[2];
         GameObject obj = Instantiate(_validationDelete, _parentValidation);
         _btns = obj.GetComponentsInChildren<Button>();
-        _btns[0].onClick.AddListener(delegate {ValidationSyncBtnBhvr(_btns[0].name, obj, value);});
-        _btns[1].onClick.AddListener(delegate {ValidationSyncBtnBhvr(_btns[1].name, obj, value);});
-	
-	}
+        _btns[0].onClick.AddListener(delegate { ValidationSyncBtnBhvr(_btns[0].name, obj, value); });
+        _btns[1].onClick.AddListener(delegate { ValidationSyncBtnBhvr(_btns[1].name, obj, value); });
 
-		public void ValidationSyncBtnBhvr(string res, GameObject obj, int value){
+    }
+
+    public void ValidationSyncBtnBhvr(string res, GameObject obj, int value)
+    {
 
         int r = int.Parse(res);
-        if (r!=1){
+        if (r != 1)
+        {
             DOTween.Play("bg_outSyncYes");
-			LoadAllPrefabs();
+            LoadAllPrefabs();
             StartCoroutine(waitForExitValidation(obj, true, value));
-            Debug.Log("Yes validation");   
-            
-        }else{
+            Debug.Log("Yes validation");
+
+        }
+        else
+        {
             DOTween.Play("bg_syncExit");
             Debug.Log("No validation");
             StartCoroutine(waitForExitValidation(obj, false, value));
@@ -129,11 +141,11 @@ public class Ctrl_LoadProjects : MonoBehaviour {
 
     private IEnumerator DeletePrefab(GameObject obj)
     {
-        yield return new WaitForSeconds(4.0f);	
+        yield return new WaitForSeconds(4.0f);
         DestroyImmediate(obj);
     }
 
-	private IEnumerator waitForExitValidation(GameObject go, bool isDelete, int value)
+    private IEnumerator waitForExitValidation(GameObject go, bool isDelete, int value)
     {
         yield return new WaitForSeconds(0.5f);
         DestroyImmediate(go);
@@ -141,23 +153,26 @@ public class Ctrl_LoadProjects : MonoBehaviour {
         {
             GameObject obj = Instantiate(_msgDelete, _textParent);
             StartCoroutine(DeletePrefab(obj));
-			var result = _projectServices.DeleteProject (_projects[value]);
+            var result = _projectServices.DeleteProject(_projects[value]);
         }
     }
-	
-    public void backToScene(){
-		StartCoroutine (BackOne ());
-	}
 
-	IEnumerator BackOne(){
-		DOTween.Play("bg_transition");
-		yield return new WaitForSeconds(1.0f);
-		SceneManager.LoadScene ("CreateViewPj");
-	}
+    public void backToScene()
+    {
+        StartCoroutine(BackOne());
+    }
 
-	public IEnumerator goToScene(){
-		yield return new WaitForSeconds(1.0f);
-		SceneManager.LoadScene ("Challenge_HUD");
-	}
+    IEnumerator BackOne()
+    {
+        DOTween.Play("bg_transition");
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene("CreateViewPj");
+    }
+
+    public IEnumerator goToScene()
+    {
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene("Challenge_HUD");
+    }
 
 }
