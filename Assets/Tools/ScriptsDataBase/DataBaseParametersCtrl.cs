c4b9c76f5f18658cd0eb061276d850931234ccfb
Skauron <43181268+Skauron@users.noreply.github.com>
2020-10-02 11:50:12 -0500

    Update

--- Assets/Tools/ScriptsDataBase/DataBaseParametersCtrl.cs
+++ Assets/Tools/ScriptsDataBase/DataBaseParametersCtrl.cs
@@ -8,6 +8,7 @@ using System.Security.Cryptography;
 using System.Security;
 using System.Text;
 using UnityEngine.SceneManagement;
+using Mono.Data.Sqlite;
 
 public class DataBaseParametersCtrl : MonoBehaviour
 {
@@ -103,6 +104,11 @@ public class DataBaseParametersCtrl : MonoBehaviour
             isNotTeacherExist = false;
 			isSyncNot = false;
             Salt = "EHS-dpa";
+            #if UNITY_WEBGL
+                //StartCoroutine(RunDbCode("dtdbtemplate.db"));
+            #else
+                //_dataServices = new DataService("dtdbtemplate.db");
+            #endif
             _dataServices = new DataService("dtdbtemplate.db");
             _sqliteConnection = _dataServices._connection;
             isQueryOk = false;
@@ -233,5 +239,64 @@ public class DataBaseParametersCtrl : MonoBehaviour
         // }
 
         //return db.Query<Val> ("select 'Price' as 'Money', 'Time' as 'Date' from Valuation where StockId = ?", stock.Id);​
+    }
+
+    IEnumerator RunDbCode(string fileName)
+    {
+        //Where to copy the db to
+        string dbDestination = Path.Combine(Application.persistentDataPath, "data");
+        dbDestination = Path.Combine(dbDestination, fileName);
+
+        //Check if the File do not exist then copy it
+        if (!File.Exists(dbDestination))
+        {
+            //Where the db file is at
+            string dbStreamingAsset = Path.Combine(Application.streamingAssetsPath, fileName);
+
+            byte[] result;
+
+            //Read the File from streamingAssets. Use WWW for Android
+            if (dbStreamingAsset.Contains("://") || dbStreamingAsset.Contains(":///"))
+            {
+                WWW www = new WWW(dbStreamingAsset);
+                yield return www;
+                result = www.bytes;
+            }
+            else
+            {
+                result = File.ReadAllBytes(dbStreamingAsset);
+            }
+            Debug.Log("Loaded db file");
+
+            //Create Directory if it does not exist
+            if (!Directory.Exists(Path.GetDirectoryName(dbDestination)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(dbDestination));
+            }
+
+            //Copy the data to the persistentDataPath where the database API can freely access the file
+            File.WriteAllBytes(dbDestination, result);
+            Debug.Log("Copied db file");
+        }
+
+        try
+        {
+            //Tell the db final location for debugging
+            Debug.Log("DB Path: " + dbDestination.Replace("/", "\\"));
+            //Add "URI=file:" to the front of the url beore using it with the Sqlite API
+            dbDestination = "URI=file:" + dbDestination;
+
+            //Now you can do the database operation below
+            //open db connection
+            var connection = new SqliteConnection(dbDestination);
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            Debug.Log("Success!");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Failed: " + e.Message);
+        }
     }
 }
--- Assets/Tools/ScriptsDataBase/DataService.cs
+++ Assets/Tools/ScriptsDataBase/DataService.cs
@@ -1,24 +1,29 @@
 ﻿using SQLite4Unity3d;
 using UnityEngine;
 using System;
+using System.Collections;
+using System.IO;
+using Mono.Data.Sqlite;
 #if !UNITY_EDITOR
 using System.Collections;
 using System.IO;
 #endif
 using System.Collections.Generic;
 
-public class DataService  {
+public class DataService
+{
 
-	public SQLiteConnection _connection;
-	private SQLiteException _exception;
+    public SQLiteConnection _connection;
+    private SQLiteException _exception;
 
-	private bool _isFirstTime = false;
+    private bool _isFirstTime = false;
 
-	public DataService(string DatabaseName){
+    public DataService(string DatabaseName)
+    {
 
-		#if UNITY_EDITOR
-				var dbPath = string.Format(@"Assets/StreamingAssets/{0}", DatabaseName);
-		#else
+#if UNITY_EDITOR
+        var dbPath = string.Format(@"Assets/StreamingAssets/{0}", DatabaseName);
+#else
         // check if file exists in Application.persistentDataPath
         var filepath = string.Format("{0}/{1}", Application.persistentDataPath, DatabaseName);
 
@@ -28,55 +33,55 @@ public class DataService  {
             // if it doesn't ->
             // open StreamingAssets directory and load the db ->
 
-			#if UNITY_ANDROID 
+#if UNITY_ANDROID
 						var loadDb = new WWW("jar:file://" + Application.dataPath + "!/assets/" + DatabaseName);  // this is the path to your StreamingAssets in android
 						while (!loadDb.isDone) { }  // CAREFUL here, for safety reasons you shouldn't let this while loop unattended, place a timer and error check
 						// then save to Application.persistentDataPath
 						File.WriteAllBytes(filepath, loadDb.bytes);
 						_isFirstTime = true;
 
-			#elif UNITY_IOS
+#elif UNITY_IOS
 							var loadDb = Application.dataPath + "/Raw/" + DatabaseName;  // this is the path to your StreamingAssets in iOS
 							// then save to Application.persistentDataPath
 							File.Copy(loadDb, filepath);
-			#elif UNITY_WP8
+#elif UNITY_WP8
 							var loadDb = Application.dataPath + "/StreamingAssets/" + DatabaseName;  // this is the path to your StreamingAssets in WindowsPhone
 							// then save to Application.persistentDataPath
 							File.Copy(loadDb, filepath);
 
-			#elif UNITY_WINRT
+#elif UNITY_WINRT
 					var loadDb = Application.dataPath + "/StreamingAssets/" + DatabaseName;  // this is the path to your StreamingAssets in ---
 					// then save to Application.persistentDataPath
 					File.Copy(loadDb, filepath);
 					
-			#elif UNITY_STANDALONE_OSX
+#elif UNITY_STANDALONE_OSX
 					var loadDb = Application.dataPath + "/Resources/Data/StreamingAssets/" + DatabaseName;  // this is the path to your StreamingAssets in OSX
 					// then save to Application.persistentDataPath
 					File.Copy(loadDb, filepath);
-			#else
-				var loadDb = Application.dataPath + "/StreamingAssets/" + DatabaseName;  // this is the path to your StreamingAssets in PC
+#else
+				// var loadDb = Application.dataPath + "/StreamingAssets/" + DatabaseName;  // this is the path to your StreamingAssets in PC
 				
-				string result;
-				if (loadDb.Contains("://") || loadDb.Contains(":///"))
-				{
-					WWW www = new WWW(loadDb);
-					yield return www;
-					result = www.text;
-				}
-				// then save to Application.persistentDataPath
-				File.Copy(result, filepath);
+				// string result;
+				// if (loadDb.Contains("://") || loadDb.Contains(":///"))
+				// {
+				// 	WWW www = new WWW(loadDb);
+				// 	yield return www;
+				// 	result = www.text;
+				// }
+				// // then save to Application.persistentDataPath
+				// File.Copy(result, filepath);
 
-			#endif
+#endif
 
 					Debug.Log("Database written");
 
 			}
 
 					var dbPath = filepath;
-		#endif
+#endif
         _connection = new SQLiteConnection(dbPath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
-        Debug.Log("Final PATH: " + dbPath); 
+        Debug.Log("Final PATH: " + dbPath);
 
-	}
-	
+    }
+
 }
